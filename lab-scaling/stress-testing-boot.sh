d6f8a1ef292786b305069ef92d1a049941f3c362
Mark Sage <38619160+idaho-guy@users.noreply.github.com>
2019-09-06 08:36:45 -0500

    Update stress-testing-boot.sh

--- lab-scaling/stress-testing-boot.sh
+++ lab-scaling/stress-testing-boot.sh
@@ -2,5 +2,6 @@
 # USAGE: Place in in the User Data section (under Advanced) when launching an EC2 instance
 # DESCRIPTION: After updating from the repo, installs stress, a tool used to create various system load for testing purposes.
 yum update -y
+sudo amazon-linux-extras install epel
 yum install stress -y
 /usr/bin/stress --cpu 8 --timeout 10m
